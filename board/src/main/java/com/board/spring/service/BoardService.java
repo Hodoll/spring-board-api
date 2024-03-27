7eeef49b2de6d217e79e3cee364bf1c562172203
Spamdong <Spamdong@DESKTOP-QG20CI2>
2024-03-27 19:22:02 +0900

    refactor : Add @Transactional
    트랜잭션 어노테이션 추가

--- board/src/main/java/com/board/spring/service/BoardService.java
+++ board/src/main/java/com/board/spring/service/BoardService.java
@@ -3,10 +3,12 @@ package com.board.spring.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.board.spring.mapper.BoardMapper;
 import com.board.spring.vo.BoardVO;
 
+@Transactional
 @Service
 public class BoardService {
 	
