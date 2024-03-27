89983b32dfaa92abb672f75b6118706467502a17
Spamdong <Spamdong@DESKTOP-QG20CI2>
2024-03-27 20:06:33 +0900

    refactor : BoardVo 어노테이션 수정
    @DATA => @Getter, @Setter로 수정

--- board/src/main/java/com/board/spring/vo/BoardVO.java
+++ board/src/main/java/com/board/spring/vo/BoardVO.java
@@ -1,8 +1,10 @@
 package com.board.spring.vo;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class BoardVO {
 	private String contentId;
 	private String contentName;
