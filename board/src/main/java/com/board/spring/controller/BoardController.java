de639e890a32356a80736f3d3d6e0b4116d17f18
Spamdong <Spamdong@DESKTOP-QG20CI2>
2024-03-27 19:25:11 +0900

    refactor : add RequestMapping
    컨트롤러 requestMapping 어노테이션 추가

--- board/src/main/java/com/board/spring/controller/BoardController.java
+++ board/src/main/java/com/board/spring/controller/BoardController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ import com.board.spring.vo.BoardVO;
 import ch.qos.logback.classic.Logger;
 
 @RestController
+@RequestMapping("/board")
 public class BoardController {
 	private final Logger logger = (Logger) LoggerFactory.getLogger(getClass());
 	//# AutoWired를 지웠을때 @GetMapping("/board/select") url 입력시 에러가 났음. @Service 어노테이션이 선언 된 상태에서
@@ -32,7 +34,7 @@ public class BoardController {
 	private BoardService boardService;
 	
 	//전체리스트 조회
-    @GetMapping("/board/list")
+    @GetMapping("/list")
     public List<?> getContents(@RequestParam(value="ORDER_BY",  required = false, defaultValue="Y")  String ORDER_BY){
     	BoardVO boardVO = new BoardVO();
     	boardVO.setORDER_BY(ORDER_BY);
@@ -40,14 +42,14 @@ public class BoardController {
     	return boardService.selectBoard(boardVO);
     }
 	//개별리스트디테일 조회
-    @GetMapping("/board/one")
+    @GetMapping("/one")
     public List<?> getDetailContents(@RequestBody Map<String, String> param){
     	BoardVO boardVO = new BoardVO();
     	boardVO.setCONTENT_ID(param.get("CONTENT_ID"));
     	return boardService.selectDetail(boardVO);
     }
 	//저장    
-    @PostMapping("/board/add")
+    @PostMapping("/add")
     public void addContents(@RequestBody Map<String, String> param) {
     	//System.out.println는 좋지 않으나, logger.Debug에서 에러메시지 출력이 안됨.. 이유는 모르겠다.. 추후 질문 필요
     	System.out.println("******************************"  + param);
@@ -56,7 +58,7 @@ public class BoardController {
     	boardService.add(boardVO);
     }
 	//수정    
-    @PutMapping("/board/update/{CONTENT_ID}")
+    @PutMapping("/update/{CONTENT_ID}")
     public void updateContents(@PathVariable("CONTENT_ID") String CONTENT_ID, @RequestBody Map<String, String> param) {
     	System.out.println("*********" + CONTENT_ID);
     	System.out.println("*********" + param);
@@ -67,7 +69,7 @@ public class BoardController {
     	boardService.modify(boardVO);
     }
     //삭제
-    @DeleteMapping("/board/delete/{CONTENT_ID}")
+    @DeleteMapping("/delete/{CONTENT_ID}")
     public void deleteContents(@PathVariable("CONTENT_ID") String CONTENT_ID) {
     	System.out.println("*********" + CONTENT_ID);
     	BoardVO boardVO = new BoardVO();
