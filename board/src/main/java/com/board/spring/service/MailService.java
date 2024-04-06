394e48b7ec2b15bcdbdffa91b2f89de77ca265ea
호진 <ghwls236@gmail.com>
2024-04-06 21:45:53 +0900

    feat : Mailservice 생성

---
+++ board/src/main/java/com/board/spring/service/MailService.java
@@ -0,0 +1,10 @@
+package com.board.spring.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class MailService {
+
+}
