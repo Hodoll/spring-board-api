ce0ed43e1cc2c268d491332b8aead31237d619a9
호진 <ghwls236@gmail.com>
2024-04-07 04:12:42 +0900

    feat : emailVO 생성

---
+++ board/src/main/java/com/board/spring/vo/MailVO.java
@@ -0,0 +1,13 @@
+package com.board.spring.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class  MailVO {
+	private String contentTo;
+	private String contentFrom;
+	private String contentName;
+	private String contentDelDt;
+}
