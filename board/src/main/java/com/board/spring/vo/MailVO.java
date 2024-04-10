5085abb1e7b45f557f3fdb4bb2d68700d9de046d
호진 <ghwls236@gmail.com>
2024-04-10 17:38:34 +0900

    remove : MailVO 삭제

--- board/src/main/java/com/board/spring/vo/MailVO.java
+++
@@ -1,8 +0,0 @@
-package com.board.spring.vo;
-
-public class  MailVO {
-	 public static String contentTo;
-	 public static String contentFrom;
-	 public static String contentName;
-	 public static String contentDelDt;
-}
