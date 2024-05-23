042eb9ee785b66d4fa98aa798f5569c7ceef1753
호진 <ghwls236@gmail.com>
2024-05-23 22:40:07 +0900

    chore : jpa 설정 ing..

---
+++ board2/src/main/java/com/board/spring/controller/BoardController.java
@@ -0,0 +1,13 @@
+package com.board.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class BoardController {
+	@GetMapping("/list")
+	public String list() {
+		return "test";
+	} 
+}
